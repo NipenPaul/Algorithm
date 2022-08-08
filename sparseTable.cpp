a9dd8c5bdb815b5a07b70f63c142b4d47a60e148
NipenPaul <53397507+NipenPaul@users.noreply.github.com>
2022-08-08 19:57:40 +0600

    Update sparseTable.cpp

--- sparseTable.cpp
+++ sparseTable.cpp
@@ -3,43 +3,38 @@
 using namespace std;
 
 const int LIM = (int) 2e5+2;
-int v[LIM];
-int sparse[LIM][25];
-int n;
-void sparseTableBuild()
-{
+int v[LIM], sparse[LIM][25], n, lg[LIM+1];
+
+void sparseTableBuild(){
+	lg[1] = 0;
+	for (int i = 2; i <= LIM; i++)
+    	lg[i] = lg[i/2] + 1;
 	for(int i = 1; i <= n; i++)
 		sparse[i][0] = v[i];
-	int len = log2(n);
-	for(int j = 1; j <= len; j++)
-	{
+	int len = lg[n];
+	for(int j = 1; j <= len; j++){
 		int pw = 1 << j;
-		for(int i = 1; i <= n; i++)
-		{
+		for(int i = 1; i <= n; i++){
 			if((i+pw-1) > n)break;
 			sparse[i][j] = min(sparse[i][j-1], sparse[i+pw/2][j-1]);
 		}
 	}
 }
 
-int querySparse(int l, int r)
-{
+int querySparse(int l, int r){
 	int len = r - l + 1;
-	int base = log2(len);
+	int base = lg[len];
 	int pw = 1 << base;
 	return min(sparse[l][base], sparse[r-pw+1][base]);
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 	int q; cin >> n >> q;
-	for (int i = 1; i <= n; ++i)
-	{
+	for (int i = 1; i <= n; ++i){
 		cin >> v[i];
 	}
 	sparseTableBuild();
-	while(q--)
-	{
+	while(q--){
 		int l, r; cin >> l >> r;
 		cout << querySparse(l, r) << '\n';
 	}

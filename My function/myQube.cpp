8d87bc1d889941c69f644e49af5d6ea9fc5aa761
NipenPaul <53397507+NipenPaul@users.noreply.github.com>
2022-12-08 16:37:18 +0600

    Create myQube.cpp

---
+++ My function/myQube.cpp
@@ -0,0 +1,14 @@
+template<typename T>
+T myqube(T n){
+	T lo = 0.0, hi = (T)1e9+1, ans = 0.0;
+	for(int i = 0; i < 100; i++){
+		T mid = (lo+hi)/2.0;
+		if(mid*mid*mid <= n){
+			ans = mid;
+			lo = mid;
+		}else{
+			hi = mid;
+		}
+	}
+	return ans;
+}
